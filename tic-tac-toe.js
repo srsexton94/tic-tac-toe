1245d48b21878310953c277568b428ede1c7fc22
srsexton94 <srsexton94@gmail.com>
2025-05-05 20:27:11 -0400

    finish tic-tac-toe game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,18 @@ const prompt = require("prompt-sync")();
 
 console.log("Tic Tac Toe");
 
+// Constant Reference Variables
+const WINNING_COMBOS = [
+  [0, 1, 2], // top row
+  [3, 4, 5], // middle row
+  [6, 7, 8], // bottom row
+  [0, 3, 6], // left column
+  [1, 4, 7], // middle column
+  [2, 5, 8], // right column
+  [0, 4, 8], // LTR diagonal
+  [2, 4, 6], // RTL diagonal
+];
+
 // Model data
 // Our game start variables
 let board, turn, winner;
@@ -27,6 +39,7 @@ function gameplayLoop() {
     if (isValid) {
       board[choice] = turn;
       changeTurn();
+      winner = checkWinner();
       render();
     }
   }
@@ -38,6 +51,19 @@ function checkUserChoice(choice) {
 
 function changeTurn() {
   turn = turn === "X" ? "O" : "X";
+}
+
+function checkWinner() {
+  for (let combo of WINNING_COMBOS) {
+    if (
+      board[combo[0]] &&
+      board[combo[0]] === board[combo[1]] &&
+      board[combo[1]] === board[combo[2]]
+    ) {
+      return board[combo[0]];
+    }
+  }
+  return board.some((space) => space === null) ? null : "T";
 }
 
 // View functions
@@ -56,7 +82,13 @@ function renderBoard() {
 }
 
 function renderMessage() {
-  console.log(`It is ${turn}'s turn, please choose a space.`);
+  const message =
+    winner === "T"
+      ? "It's a tie game!"
+      : winner
+      ? `Congrats, Player ${winner} has won the game!`
+      : `It is ${turn}'s turn, please choose a space.`;
+  console.log(message);
 }
 
 // Start game
